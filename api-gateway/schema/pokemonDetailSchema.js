06ed1f1a35b25ff9b86c7dacef9a901450e8e686
ridhasidi <mulyawan.rdhsd@gmail.com>
2022-03-31 19:54:40 +0700

    create api-getaway

---
+++ api-gateway/schema/pokemonDetailSchema.js
@@ -0,0 +1,90 @@
+const { gql } = require("apollo-server");
+const axios = require("axios");
+
+const typeDefs = gql`
+  type PokemonDetail {
+    id: ID
+    name: String
+    height: Int
+    weight: Int
+    photoUrl: String
+    abilities: [Ability]
+    stats: [BaseStat]
+    types: [Types]
+    moves: [Moves]
+    message: String
+  }
+
+  type Moves {
+    name: String
+  }
+
+  type BaseStat {
+    base_stat: Int
+    name: String
+  }
+
+  type Ability {
+    name: String
+    isHidden: Boolean
+  }
+  extend type Query {
+    getOnePokemon(id: ID): PokemonDetail
+  }
+`;
+
+const baseUrl = "https://pokeapi.co/api/v2/pokemon";
+
+const resolvers = {
+  Query: {
+    getOnePokemon: async (_, args) => {
+      try {
+        const { id } = args;
+        const { data } = await axios({
+          method: "GET",
+          url: baseUrl + `/${id}`,
+        });
+        const abilities = data.abilities.map((e) => {
+          return { name: e.ability.name };
+        });
+        const stats = data.stats.map((e) => {
+          return {
+            name: e.stat.name,
+            base_stat: e.base_stat,
+          };
+        });
+        const types = data.types.map((e) => {
+          return {
+            slot: e.slot,
+            name: e.type.name,
+          };
+        });
+        const moves = data.moves.map((e) => {
+          return {
+            name: e.move.name,
+          };
+        });
+        const photoUrl = data?.sprites.other["official-artwork"]["front_default"];
+        const result = {
+          id: data.id,
+          name: data.name,
+          height: data.height,
+          weight: data.weight,
+          photoUrl,
+          abilities,
+          stats,
+          types,
+          moves,
+        };
+        return result;
+      } catch (error) {
+        return error.response.data;
+      }
+    },
+  },
+};
+
+module.exports = {
+  typeDefs,
+  resolvers,
+};
--- api-gateway/schema/pokemonsSchema.js
+++ api-gateway/schema/pokemonsSchema.js
@@ -1,9 +1,61 @@
 const { gql } = require("apollo-server");
 const axios = require("axios");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type Pokemon {
+    id: ID
+    name: String
+    types: [Types]
+    photoUrl: String
+  }
 
-const resolvers = {};
+  type Types {
+    name: String
+    slot: Int
+  }
+
+  type Query {
+    getPokemons: [Pokemon]
+  }
+`;
+
+const baseUrl = "https://pokeapi.co/api/v2/pokemon";
+
+const resolvers = {
+  Query: {
+    getPokemons: async () => {
+      try {
+        const { data } = await axios({
+          method: "GET",
+          url: baseUrl,
+        });
+        const result = await Promise.all(
+          data.results.map(async (e) => {
+            const resp = await axios({
+              method: "GET",
+              url: e.url,
+            });
+            const types = resp?.data.types.map((e) => {
+              return {
+                slot: e.slot,
+                name: e.type.name,
+              };
+            });
+            return {
+              id: resp.data.id,
+              name: resp.data.name,
+              photoUrl: resp?.data?.sprites.other["official-artwork"]["front_default"],
+              types,
+            };
+          })
+        );
+        return result;
+      } catch (error) {
+        return error.response.data;
+      }
+    },
+  },
+};
 
 module.exports = {
   typeDefs,
